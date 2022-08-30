220836d9323c69aa586d2fcdf871f14bee885c18
Ushitora Anqou <ushitora@anqou.net>
2022-08-30 20:20:34 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
-#!/bin/bash -xeu
+#!/bin/bash -eu
 
 git clone https://github.com/ushitora-anqou/daidoquer2-docker-compose.git
 cd daidoquer2-docker-compose
 cp config/env.sample config/env
 cp config/runtime.exs.sample config/runtime.exs
+echo -n "Enter your Discord token > "
+read token
+sed -i -r "s/^DISCORD_TOKEN.+\$/DISCORD_TOKEN='$token'/" config/env
